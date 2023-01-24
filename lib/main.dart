aa62eedd92e26a8966c66c8e55a159340d5ffcca
mudassarwurfel <programmingwithhassan@gmail.comm>
2023-01-24 18:27:51 +0500

    Compete Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,273 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MoviApp(),
+    ));
+
+class MoviApp extends StatefulWidget {
+  const MoviApp({Key? key}) : super(key: key);
+
+  @override
+  _MoviAppState createState() => _MoviAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _MoviAppState extends State<MoviApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color(0xff1C262F),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Color(0xFF1B2C3B),
+        elevation: 0.0,
+        title: Text("Niche Movies"),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ))
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: EdgeInsets.only(top: 12.0),
+        child: ListView(
+          children: [
+            //child 1
+            Container(
+              height: 40.0,
+              width: double.infinity,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Chip(
+                      label: Text(
+                        "All",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Colors.orange,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Chip(
+                      label: Text(
+                        "Action",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Chip(
+                      label: Text(
+                        "Adventure",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Chip(
+                      label: Text(
+                        "Comedy",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: Text(
+                    "Top Trending",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange,
+                      fontSize: 26,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              width: double.infinity,
+              height: 230,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: MoviCard("The Outside War", "8.5/10",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5vgalDqbwc_eCLUntHbBNwTjHBgiHg4zfunzziKr0RA&s"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: MoviCard("The Outside War", "8.5/10",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0BteRF7tYFK6E1E9iO_-m8Qk9j33Iqd4MKKNjzsy3x3Pt8iz-d7GyIx89oOZpvR2ywe0&usqp=CAU"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: MoviCard("The Outside War", "8.5/10",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5vgalDqbwc_eCLUntHbBNwTjHBgiHg4zfunzziKr0RA&s"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: MoviCard("The Outside War", "8.5/10",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0BteRF7tYFK6E1E9iO_-m8Qk9j33Iqd4MKKNjzsy3x3Pt8iz-d7GyIx89oOZpvR2ywe0&usqp=CAU"),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 12),
+                  child: Text(
+                    "Recommended Movies",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange,
+                      fontSize: 26,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              width: double.infinity,
+              height: 230,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MoviCard("Joker", "8.5/10",
+                        'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MoviCard("Joker", "8.5/10",
+                        'https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_512.gif'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MoviCard("Joker", "8.5/10",
+                        'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: MoviCard("Joker", "8.5/10",
+                        'https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_512.gif'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF1B2C3B) ,
+        fixedColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+              icon: IconButton(
+            icon: Icon(Icons.home,),
+            onPressed: () {},
+          ),
+            label: "HOME",
+
+          ),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              label:  "SEARCH",
+
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(Icons.download),
+              onPressed: () {},
+            ),
+            label:  "DOWNLOAD",
+
+          ),
+
+
+
+        ],
+      ),
+    );
+  }
+
+  Widget MoviCard(String Title, String Rate, String imgPath) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          Card(
+            elevation: 0.0,
+            child: Image.network(
+              imgPath,
+              fit: BoxFit.fill,
+              width: 130.0,
+              height: 160.0,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            Title,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            Rate,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
