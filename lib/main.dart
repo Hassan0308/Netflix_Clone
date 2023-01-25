e44a6805238973a3b2367d5c1294308f2b138ac5
mudassarwurfel <programmingwithhassan@gmail.comm>
2023-01-25 19:58:50 +0500

    My Top 3 Movies Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
+class Data {
+  String? url;
+  String? name;
+  String? description;
+
+  Data(@required this.url, @required this.name, @required this.description);
+}
+
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MoviApp(),
     ));
 
 class MoviApp extends StatefulWidget {
+  static List _data = [
+    Data(
+        "https://wwwimage-us.pplusstatic.com/thumbnails/photos/w370-q80/movie_asset/59/74/33/sml_salone_poster_1400x2100.jpg",
+        "Popular Movie (SMILE)",
+        "Our 2023 CLASSIC Refillable Popcorn Bucket is now available! Pop into an AMC Theatres and enjoy refills all year long – only 5.29+tax. Plus, AMC Stubs® members get their 2nd same-day refill for FREE."
+    ),
+    Data(
+        "https://static.hbo.com/2022-12/black-adam-ka-1920.jpg",
+        "BLACK ADAM",
+        "Our 2023 CLASSIC Refillable Popcorn Bucket is now available! Pop into an AMC Theatres and enjoy refills all year long – only 5.29+tax. Plus, AMC Stubs® members get their 2nd same-day refill for FREE."
+    ),
+    Data(
+        "https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_465,q_auto,w_310/v1669911582/amc-cdn/production/2/movies/65800/65813/PosterDynamic/146530.jpg",
+        "MEGAN",
+        "Our 2023 CLASSIC Refillable Popcorn Bucket is now available! Pop into an AMC Theatres and enjoy refills all year long – only 5.29+tax. Plus, AMC Stubs® members get their 2nd same-day refill for FREE."),
+
+
+  ];
+
   const MoviApp({Key? key}) : super(key: key);
 
   @override
@@ -19,215 +46,60 @@ class _MoviAppState extends State<MoviApp> {
       backgroundColor: Color(0xff1C262F),
       appBar: AppBar(
         backgroundColor: Color(0xFF1B2C3B),
-        elevation: 0.0,
-        title: Text("Niche Movies"),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.more_vert,
-                color: Colors.white,
-              ))
-        ],
+        title: Text("My Top 3 Movies"),
+        centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.only(top: 12.0),
-        child: ListView(
-          children: [
-            //child 1
-            Container(
-              height: 40.0,
-              width: double.infinity,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Chip(
-                      label: Text(
-                        "All",
-                        style: TextStyle(
-                          color: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+          child: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            itemCount: MoviApp._data.length,
+            itemBuilder: (context, index) {
+              Data _myData = MoviApp._data[index];
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DetailPage(_myData)));
+                  },
+                  child: Container(
+
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          _myData.url.toString(),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 250,
+
+
                         ),
-                      ),
-                      backgroundColor: Colors.orange,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Chip(
-                      label: Text(
-                        "Action",
-                        style: TextStyle(
-                          color: Colors.white,
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              _myData.name.toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      backgroundColor: Colors.blueGrey,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Chip(
-                      label: Text(
-                        "Adventure",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      backgroundColor: Colors.blueGrey,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Chip(
-                      label: Text(
-                        "Comedy",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      backgroundColor: Colors.blueGrey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Row(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.only(left: 12),
-                  child: Text(
-                    "Top Trending",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange,
-                      fontSize: 26,
+
+
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Container(
-              width: double.infinity,
-              height: 230,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: MoviCard("The Outside War", "8.5/10",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5vgalDqbwc_eCLUntHbBNwTjHBgiHg4zfunzziKr0RA&s"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: MoviCard("The Outside War", "8.5/10",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0BteRF7tYFK6E1E9iO_-m8Qk9j33Iqd4MKKNjzsy3x3Pt8iz-d7GyIx89oOZpvR2ywe0&usqp=CAU"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: MoviCard("The Outside War", "8.5/10",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5vgalDqbwc_eCLUntHbBNwTjHBgiHg4zfunzziKr0RA&s"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: MoviCard("The Outside War", "8.5/10",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0BteRF7tYFK6E1E9iO_-m8Qk9j33Iqd4MKKNjzsy3x3Pt8iz-d7GyIx89oOZpvR2ywe0&usqp=CAU"),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 12),
-                  child: Text(
-                    "Recommended Movies",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange,
-                      fontSize: 26,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Container(
-              width: double.infinity,
-              height: 230,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: MoviCard("Joker", "8.5/10",
-                        'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: MoviCard("Joker", "8.5/10",
-                        'https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_512.gif'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: MoviCard("Joker", "8.5/10",
-                        'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: MoviCard("Joker", "8.5/10",
-                        'https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_512.gif'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF1B2C3B) ,
-        fixedColor: Colors.white,
-        items: [
-          BottomNavigationBarItem(
-              icon: IconButton(
-            icon: Icon(Icons.home,),
-            onPressed: () {},
-          ),
-            label: "HOME",
-
-          ),
-          BottomNavigationBarItem(
-              icon: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              ),
-              label:  "SEARCH",
-
-          ),
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: Icon(Icons.download),
-              onPressed: () {},
-            ),
-            label:  "DOWNLOAD",
-
-          ),
-
-
-
-        ],
-      ),
+              );
+            },
+          )),
     );
   }
 
@@ -267,6 +139,68 @@ class _MoviAppState extends State<MoviApp> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final Data _data;
+
+  DetailPage(this._data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff1C262F),
+      appBar: AppBar(
+        title: Text("Detail Page"),
+        centerTitle: true,
+        backgroundColor: Color(0xFF1B2C3B),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+         child: Column(
+           children: [
+             Image.network(
+               _data.url.toString(),
+               fit: BoxFit.cover,
+               width: double.infinity,
+               height: 300,
+
+
+             ),
+             SizedBox(height: 16,),
+             Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text(
+                   _data.name.toString(),
+                   style: TextStyle(
+                     color: Colors.white,
+                     fontSize: 24,
+                   ),
+                 ),
+                 SizedBox(height: 10,),
+                 Text(
+                   _data.description.toString(),
+                   style: TextStyle(
+                     color: Colors.white,
+                     fontSize: 16,
+                   ),
+                 ),
+
+               ],
+             ),
+SizedBox(height: 16,),
+
+ElevatedButton(onPressed: (){}, child: Text("WATCH NOW!"),style: ElevatedButton.styleFrom(
+  backgroundColor: Colors.orange
+),)
+           ],
+         ),
+        ),
       ),
     );
   }
